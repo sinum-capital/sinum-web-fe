31c7b551cb35a71833f107fadf471b194eef8fb6
andrew_f <andrijfodor@gmail.com>
2024-11-04 01:07:04 +0200

    implemented border-radius, gradient border, general props

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
+"use client";
 import { CustomButton } from "@/ui-components/CustomButton";
 import styles from "./page.module.scss";
+import icon from "@/images/backArrowWhite.svg";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        Main. Test components here
-        Buttons:
+        Main. Test components here Buttons:
         <div>
-          <CustomButton/>
+          <CustomButton
+          borderRadius="r3"
+          gradientBorder="b1"
+            text="Custom Button"
+            imageProps={{ height: 10, width: 10, iconSrc: icon }}
+            onClick={() => console.log("Button clicked!")}
+            className="my-custom-style"
+            disabled={false}
+          />
+          <CustomButton
+          borderRadius="r1"
+            text="Custom Button"
+          />
+          <CustomButton 
+            imageProps={{ height: 10, width: 10, iconSrc: icon }}
+            />
         </div>
       </main>
-      <footer className={styles.footer}>
-        Footer
-      </footer>
+      <footer className={styles.footer}>Footer</footer>
     </div>
   );
 }
--- ui-components/CustomButton/CustomButton.tsx
+++ ui-components/CustomButton/CustomButton.tsx
@@ -1,6 +1,60 @@
-export const CustomButton = () => {
-    return <div>
-        <p>Image</p>
-        <button>Button</button>
-    </div>
+import Image from "next/image";
+import styles from "./CustomButton.module.scss";
+
+//r1 - border-radius=8px, r2 - 12px, r3 - 30px
+//b1 - gradient border 2px with set border-redius
+
+interface CustomButtonProps {
+  text?: string;
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+  wrapperStyle?: React.CSSProperties;
+  imageProps?: {
+    iconSrc: string;
+    icopAlt?: string;
+    imageStyles?: React.CSSProperties;
+    width: number;
+    height: number;
+  };
+  borderRadius?: "r1" | "r2" | "r3";
+  gradientBorder?: "b1";
 }
+
+export const CustomButton: React.FC<CustomButtonProps> = ({
+  text,
+  imageProps,
+  onClick,
+  className = "",
+  disabled = false,
+  wrapperStyle,
+  borderRadius,
+  gradientBorder,
+}) => {
+  return (
+    <div
+      className={`${styles["button-wrapper"]} ${className}`}
+      style={wrapperStyle}
+    >
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        className={`${styles["custom-button"]} ${
+          borderRadius ? styles[borderRadius] : ""
+        } ${gradientBorder ? styles[gradientBorder] : ""}`}
+      >
+        {imageProps && (
+          <Image
+            src={imageProps.iconSrc}
+            alt={imageProps.icopAlt || "sinum-image"}
+            width={imageProps.width}
+            height={imageProps.height}
+            className={styles["button-icon"]}
+            style={imageProps.imageStyles || {}}
+          />
+        )}
+        {text && <p>{text}</p>}
+      </button>
+    </div>
+  );
+};
