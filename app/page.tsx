285ecc85adb972efa61bbc6b13098ac8a6b256be
Qvendsh bc2024-2 <qvendsh@gmail.com>
2024-11-06 22:13:58 +0200

    created toggle-switch component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.scss";
+import Switch from "@/ui-components/Switch/Switch";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        Main. Test components here
+        <h2>switch</h2>
+          <Switch/>
       </main>
       <footer className={styles.footer}>
         Footer
